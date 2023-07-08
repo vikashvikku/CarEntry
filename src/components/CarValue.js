20a07054ce74b131ce10dfe26e33d7f263c19db3
Vikash Kumar <vikkuvikash79097@gmail.com>
2023-07-09 00:28:04 +0530

    initial Commit

---
+++ src/components/CarValue.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const CarValue = () => {
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) => {
+      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    // let cost = 0;
+    // for (let car of filteredCars) {
+    //   cost += car.cost;
+    // }
+    // return cost;
+
+    return filteredCars.reduce((acc, car) => {
+      return acc + car.cost;
+    }, 0);
+  });
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
+};
+
+export default CarValue;
